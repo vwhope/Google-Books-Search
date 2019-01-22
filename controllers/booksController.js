7a7919d6ebecae0d0c14ef92265e4bbb4ef6394c
Grover <41809783+vwhope@users.noreply.github.com>
2019-01-22 11:11:53 -0500

    booksController - correct findAll params

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -5,7 +5,7 @@ const db = require("../models");
 module.exports = {
   findAll: function(req, res) {
     db.Book
-      .find(req, res)
+      .find(req.query)
       .sort({ title: 1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
